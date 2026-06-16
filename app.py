2f08d755441dc776ee04c0d4cf4ffe2fa32068fa
claypotdat01-blip <claypotdat01@gmail.com>
2026-06-16 17:06:03 +0700

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ st.set_page_config(
     page_title="Ocean Health & Fisheries Dashboard",
     layout="wide"
 )
+if "page" not in st.session_state:
+    st.session_state.page = "home"
 
+if "role" not in st.session_state:
+    st.session_state.role = None
 # =========================
 # LOAD DATA
 # =========================
@@ -18,13 +22,97 @@ def load_data():
     return df
 
 df = load_data()
+if st.session_state.page == "home":
 
+    st.title("🌊 Platform Informasi & Prediksi Klimatologi Oseanografi")
+
+    c1,c2 = st.columns(2)
+
+    with c1:
+        st.image(
+            "https://cdn-icons-png.flaticon.com/512/3063/3063822.png",
+            width=150
+        )
+
+        if st.button(
+            "🐟 Masuk Sebagai Nelayan",
+            use_container_width=True
+        ):
+            st.session_state.role="nelayan"
+            st.session_state.page="dashboard"
+            st.rerun()
+
+    with c2:
+        st.image(
+            "https://cdn-icons-png.flaticon.com/512/3135/3135810.png",
+            width=150
+        )
+
+        if st.button(
+            "🎓 Masuk Sebagai Akademisi",
+            use_container_width=True
+        ):
+            st.session_state.role="akademisi"
+            st.session_state.page="dashboard"
+            st.rerun()
+
+    st.stop()
 # =========================
 # NORMALIZATION
 # =========================
 def normalize(series):
     return (series - series.min()) / (series.max() - series.min())
+mode = st.sidebar.selectbox(
+    "Mode Data",
+    [
+        "Historis",
+        "Real Time",
+        "Prediksi"
+    ]
+)
 
+indeks = st.sidebar.selectbox(
+    "Indeks",
+    [
+        "Ocean Health Index",
+        "Fisheries Index"
+    ]
+)
+if st.session_state.page == "home":
+
+    st.title("🌊 Platform Informasi & Prediksi Klimatologi Oseanografi")
+
+    c1,c2 = st.columns(2)
+
+    with c1:
+        st.image(
+            "https://cdn-icons-png.flaticon.com/512/3063/3063822.png",
+            width=150
+        )
+
+        if st.button(
+            "🐟 Masuk Sebagai Nelayan",
+            use_container_width=True
+        ):
+            st.session_state.role="nelayan"
+            st.session_state.page="dashboard"
+            st.rerun()
+
+    with c2:
+        st.image(
+            "https://cdn-icons-png.flaticon.com/512/3135/3135810.png",
+            width=150
+        )
+
+        if st.button(
+            "🎓 Masuk Sebagai Akademisi",
+            use_container_width=True
+        ):
+            st.session_state.role="akademisi"
+            st.session_state.page="dashboard"
+            st.rerun()
+
+    st.stop()
 # =========================
 # DERIVED VARIABLES
 # =========================
@@ -71,7 +159,22 @@ parameter = st.sidebar.selectbox(
         "angin_v"
     ]
 )
+mode = st.sidebar.selectbox(
+    "Mode Data",
+    [
+        "Historis",
+        "Real Time",
+        "Prediksi"
+    ]
+)
 
+indeks = st.sidebar.selectbox(
+    "Indeks",
+    [
+        "Ocean Health Index",
+        "Fisheries Index"
+    ]
+)
 # =========================
 # HEADER
 # =========================
@@ -99,7 +202,12 @@ tab1, tab2, tab3, tab4 = st.tabs(
         "🌊 Ocean Health"
     ]
 )
-
+df["year"] = df["time"].dt.year
+df["month"] = df["time"].dt.month
+tahun = st.sidebar.selectbox(
+    "Pilih Tahun",
+    sorted(df["year"].unique())
+)
 # =========================
 # TAB 1
 # =========================
@@ -133,8 +241,142 @@ with tab3:
     )
 
     st.plotly_chart(fig, use_container_width=True)
-
+musim = {
+    "Musim Barat":[12,1,2],
+    "Peralihan I":[3,4,5],
+    "Musim Timur":[6,7,8],
+    "Peralihan II":[9,10,11]
+}
+df_filter = df[df["year"] == tahun]
 # =========================
+if breakdown == "Bulanan":
+
+    bulan = st.sidebar.selectbox(
+        "Bulan",
+        [
+            "Jan","Feb","Mar","Apr",
+            "Mei","Jun","Jul","Agu",
+            "Sep","Okt","Nov","Des"
+        ]
+    )
+
+    idx = [
+        "Jan","Feb","Mar","Apr",
+        "Mei","Jun","Jul","Agu",
+        "Sep","Okt","Nov","Des"
+    ].index(bulan)+1
+
+    df_filter = df_filter[
+        df_filter["month"] == idx
+    ]
+    if breakdown == "Musiman":
+
+    musim_pilih = st.sidebar.selectbox(
+        "Musim",
+        list(musim.keys())
+    )
+
+    df_filter = df_filter[
+        df_filter["month"].isin(
+            musim[musim_pilih]
+        )
+    ]
+    lat
+lon
+
+fig = px.scatter_mapbox(
+    df_filter,
+    lat="lat",
+    lon="lon",
+    color="SOHI",
+    color_continuous_scale="RdYlGn",
+    zoom=4,
+    mapbox_style="open-street-map"
+)
+fig = px.scatter_mapbox(
+    df_filter,
+    lat="lat",
+    lon="lon",
+    color="FSI",
+    color_continuous_scale="Turbo",
+    zoom=4,
+    mapbox_style="open-street-map"
+)
+if st.session_state.role == "nelayan":
+    st.metric(
+    "Fisheries Index",
+    f"{df_filter['FSI'].mean():.1f}"
+)
+
+st.metric(
+    "Ocean Health",
+    f"{df_filter['SOHI'].mean():.1f}"
+)
+if df_filter["FSI"].mean() > 80:
+
+    st.success(
+        "🐟 Sangat Potensial untuk penangkapan ikan"
+    )
+
+elif df_filter["FSI"].mean() > 60:
+
+    st.info(
+        "🐟 Potensial untuk penangkapan ikan"
+    )
+
+else:
+
+    st.warning(
+        "⚠ Potensi tangkap rendah"
+    )
+    tab1,tab2,tab3,tab4 = st.tabs(
+    [
+        "🗺️ Spasial",
+        "📈 Time Series",
+        "📊 Statistik",
+        "🔥 Korelasi"
+    ]
+)
+    st.dataframe(
+    df_filter.describe()
+)
+
+corr = df.select_dtypes(
+    include=np.number
+).corr()
+
+fig = px.imshow(
+    corr,
+    text_auto=".2f"
+)
+
+corr = df.select_dtypes(
+    include=np.number
+).corr()
+
+fig = px.imshow(
+    corr,
+    text_auto=".2f"
+)
+
+if mode == "Real Time":
+
+    st.info(
+        "📡 Belum terhubung ke NOAA/CMEMS."
+    )
+    if mode == "Real Time":
+
+    st.info(
+        "📡 Belum terhubung ke NOAA/CMEMS."
+    )
+    trend = (
+    df.groupby("year")
+    ["SOHI"]
+    .mean()
+    .reset_index()
+)
+    from sklearn.linear_model import LinearRegression
+    
 # TAB 4
 # =========================
 with tab4:
